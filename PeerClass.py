1b2af1f147132b602b9e65b4578c5ed7711eb28f
Dominic Johnston Whiteley <dominic.johnstonwhiteley@student.manchester.ac.uk>
2021-12-24 18:41:07 +0000

    GMSG works, (group chat) live only. i can for see a problem that when a member connects after a long time the .recv(2040) command will be overloaded. Also when closing the program, it causes balcnk messages to spam the chat. perhaps some code to be executed on quit must be implemented

--- PeerClass.py
+++ PeerClass.py
@@ -2,6 +2,7 @@ import socket
 import threading
 from copy import deepcopy
 import sys
+import datetime
 ##keeping database and assocaites as objects leads to greater flexibility
 #can write methods in to database which detct ID,name,ADDR collisions,retrive data,
 #these methods may be used by other classes: for instance both client and server will likey use getHighestSCORE
@@ -9,17 +10,15 @@ import sys
 ##update scores method could be used to get an avg score from all Associates for each associate## with a maxdepth argumeant, naturally
 ##method Called on the PeerClass instance of Database
 
-class GMSG:
-  pass
-
 class Associate:
-  def __init__(self,ID = None,NAME = None,ADDR = None,SCORE = None,myAUTH = None, thAUTH = None):
+  def __init__(self,ID = None,NAME = None,ADDR = None,SCORE = None,myAUTH = None, thAUTH = None,conn = None):
     self.ID = ID
     self.NAME = NAME
     self.ADDR = ADDR
     self.SCORE = SCORE
     self.myAUTH = myAUTH
     self.thAUTH = thAUTH    
+    self.conn = conn
   def AddDatabase(self,inputs):
     self.Database = inputs
   ##more methods for associate may be useful: comparative operator, 
@@ -32,65 +31,134 @@ class Database:
         if Ass1.ADDR == Ass2.ADDR or Ass1.ID == Ass2.ID: ##and "or" or?
           retlist.remove(Ass1)
     return Database(lst = retlist)
-  
   def __init__(self,lst = []):
     self.lst = lst
-  
   def getAssociateByID(self,ID):## the getByID Commands could return instance of database.
     out = []
     for Friend in self.lst:
       if Friend.ID == ID:
         out.append(Friend)
     return out
-
   def getAssociateByNAME(self,Name):
     out = []
     for Friend in self.lst:
       if Friend.Name == Name:
         out.append(Friend)
     return out
-
   def getAssociateByADDR(self,ADDR):
     out = []
     for Friend in self.lst:
       if Friend.ADDR == ADDR:
         out.append(Friend)
     return out
-  
   def append(self,Associate):
     self.lst.append(Associate)
-
   def getTopScore(self):
     return sorted(self.lst, key= lambda Ass: Ass.SCORE)
-
   def freeID(self,ID):
     out = []
     for Friend in self.lst:
       if Friend.ID == ID:
         return False
     return True
-  
   @property
   def share(self,ID):
     string = ""
     for Ass in self.lst:
       string += ("{Ass.ID}/{Ass.ADDR}/{Ass.SCORE}")
     return string.encode("utf-8")
-
   def __init__(self,lst = None):##expects list of associates
     if lst != None:
       self.lst = lst
     else:
       self.lst = []
 
-##i think that the methods  of server should use Adresses,
+class GMSG:##easy version : weakness is that i could pretend to send a chat to everyone in the group, but single out one person instead
+  def __init__(self,peer,packit):
+    self.peer = peer
+    self.members = []
+    self.threads = []
+    self.packit = packit
+    _,_,_ ,self.datestr = packit.split(":")
+  def add(self,Associate):
+    self.members.append(Associate)
+  def listen(self,Associate):
+    while True:
+      msg = Associate.conn.recv(2040).decode("utf-8")
+      ##if self.val(msg): then print , check if everyone got the same message
+      print(f"\n{Associate.ID} {Associate.ADDR}: {msg}\n")
+  def sendMSG(self,msg):
+    for member in self.members:
+      member.conn.send(msg.encode("utf-8"))
+  def join(self):
+    for member in self.members:
+      self.threads.append(threading.Thread(target = self.listen, args = [member]))
+      self.threads[-1].start()
+    while True:
+      self.sendMSG(input("\n"))
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Server:
-  ##could have a temp databases which gets cleared every several commands. temp databses stores the details of peers which have been invovled in comms with
+  def GMSG(self,packit,conn):
+    CMD,Req,members,datestr = packit.split(":")
+    ID,ADDR = Req.split("-")
+    newAss = Associate(ID= ID,ADDR = ADDR, conn = conn)
+    myAUTH = None
+    #print("marker1")
+    if str(self.gmsg.datestr) != str(datestr) or conn == None:##act as server, recieveing 
+      print(f"recieved (first time): {ADDR}")
+      self.gmsg = GMSG(self,packit)
+      if conn != None: self.gmsg.add(newAss)
+      mystrpos = members.find(f"{str(self.ADDR)[1:-1]}")
+      mypos = members[:mystrpos].count("/")
+      memberlst = members.split("/")
+      for x in range(len(memberlst)):
+        #print(f"marker3 {x}")
+        if x%2 == 1 and mypos+x < len(memberlst):
+          ID,ADDRstr = memberlst[x+mypos].split("-")
+          ADDRstr = ADDRstr[1:-1]
+          ip,port = ADDRstr.split(",")
+          ADDR = (ip[1:-1],int(port[1:]))
+          newconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          newconn.connect(ADDR)
+          newAss = Associate(ID= ID,ADDR = ADDR, conn = newconn)
+          newpackit = f"{CMD}:{self.ID}-{self.ADDR}:{members}:{datestr}"
+          print(f"sent forwards {ADDR}")
+          newAss.conn.send(newpackit.encode("utf-8"))
+          self.gmsg.add(newAss)
+        if x%2 == 0 and mypos >= x and x > 0: 
+          ID,ADDRstr = memberlst[mypos-x].split("-")
+          ADDRstr = ADDRstr[1:-1]
+          ip,port = ADDRstr.split(",")
+          ADDR = (ip[1:-1],int(port[1:]))
+          newconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          newconn.connect(ADDR)
+          newAss = Associate(ID= ID,ADDR = ADDR, conn = newconn)
+          newpackit = f"{CMD}:{self.ID}-{self.ADDR}:{members}:{datestr}"
+          print(f"sent backwards {ADDR}")
+          newAss.conn.send(newpackit.encode("utf-8"))
+          self.gmsg.add(newAss)
+    elif conn != None:
+      print(f"recieved {newAss.ADDR}")
+      self.gmsg.add(newAss)
+
+
   def __init__(self,ID,ADDR,database = Database()):
     self.ID = ID
     self.ADDR = ADDR
     self.Database = database
-
+    self.gmsg = GMSG(None,'N:o:n:e')
   def CHANGEID(self,packit,conn):
     CMD,DATA  = packit.split(":")
     OLDID,AUTH,NEWID  = DATA.split("/")
@@ -100,11 +168,9 @@ class Server:
       self.CONFIRM(packit,conn,True)
     else:
       self.CONFIRM(packit,conn,False)
-
   def CONFIRM(self,packit,conn,Bool):
     conn.send((packit+"-"+str(Bool)).encode("utf-8"))
     conn.close()
-
   def CHANGEADDR(self,packit,conn):##needs validation
     CMD,DATA  = packit.split(":")
     ID,AUTH,ADDR  = DATA.split("/")                       ##will VALSEND be evaluated if AUTH not ok?(hopefully not)
@@ -114,15 +180,12 @@ class Server:
       self.CONFIRM(packit,conn,True)
     else:
       self.CONFIRM(packit,conn,False)
-
   def GETDATA(self,packit,conn):
     ID,auth = packit.split("/")
     conn.send("{self.Database.share}")
     conn.close()
-
   def GETINFO(self,packit):
     pass
-
   def handle_client(self):
     myserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     myserver.bind(self.ADDR)
@@ -134,6 +197,8 @@ class Server:
       CMD = packit[:packit.find(":")]
       threads.append(threading.Thread(target = self.commands[CMD], args = (packit,conn)))
       threads[-1].start()
+#      self.commands[CMD](packit,conn)
+
 
   def VALSEND(self,Associate):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -143,7 +208,6 @@ class Server:
     res = client.recv(2040).decode("utf-8")
     client.close()
     return res == validata
-
   def REG(self,packit,conn):##scoring mechanism
     CMD,DATA = packit.split(":")
     ID,thAUTH,ip,port= DATA.split("/")
@@ -154,14 +218,12 @@ class Server:
         self.Database.append(newAssociate)
         return self.CONFIRM(packit,conn,True)
     return self.CONFIRM(packit,conn,False)
-
   def VAL(self,packit,conn):
     ##should do validation checks here
     CMD,DATA = packit.split(":")
     senderID,AUTH,validata = packit.split("/")##validation such as checking auth,id and other stuff
     conn.send(validata.encode("utf-8"))
     conn.close()
-
   @property
   def commands(self):
     return {"REG":self.REG,
@@ -171,8 +233,8 @@ class Server:
               "FINDID":self.FINDID,
               "GETDATA":self.GETDATA,
               "CHANGEADDR":self.CHANGEADDR,
-              "MSG":self.MSG}
-
+              "MSG":self.MSG,
+              "GMSG":self.GMSG}
   def FINDID(self,packit,conn):
   ##every step of the line back the packit should be validated, 
   #if a server reports the packit as malicous it could report it to all serers on the line and the client which sent it
@@ -186,9 +248,7 @@ class Server:
       Id,addr = item.split("-")
       ip,port = addr.split(",")
       lst.append(Associate(ID = Id,ADDR = (ip,int(port))))
-
     relayHistory = Database(lst = lst)
-
     Requestor = Associate(ID = ReqID,thAUTH = ReqAUTH)
     try:
       packit += str(f":TGTID-{TGTID}-{str(self.Database.getAssociateByID(TGTID)[0].ADDR)}")
@@ -216,7 +276,6 @@ class Server:
           conn.send(History.encode("utf-8"))
           break 
       conn.close()
-
   def MSG(self,packit,conn):
     def send(conn):
       try: 
@@ -227,7 +286,6 @@ class Server:
         return conn.close()
       except:
         return conn.close()
-
     def recieve(conn):
       try:
         while True:
@@ -237,7 +295,6 @@ class Server:
         return conn.close()
       except:
         return conn.close()
-
     recievethread = threading.Thread(target = recieve, args = [conn]) 
     sendthread = threading.Thread(target = send, args = [conn]) 
     recievethread.start()
@@ -246,6 +303,19 @@ class Server:
     sendthread.join()
 
 class Client:
+  def GMSG(self,others):
+    ls = [f"{Associate.ID}-{Associate.ADDR}/" for Associate in others]
+    string = ''
+    string += f"{self.ID}-{self.ADDR}/"
+    for x in ls:
+      string += x
+    ##instead hand over to server method
+    mydate = str(datetime.datetime.now()).replace(":",";")
+    packit = f"GMSG:{self.ID}-{self.ADDR}:{string[:-1]}:{mydate}"
+    self.server.gmsg = GMSG(self.server,packit)
+    self.server.GMSG(packit,None)
+
+
   def MSG(self,Associate):
     def send(client):
       try:
@@ -256,7 +326,6 @@ class Client:
         client.close()
       except:
         client.close()
-
     def recieve(client):
         try:
           while True:
@@ -266,7 +335,6 @@ class Client:
           client.close()
         except:
           client.close()
-
     packit = f"MSG:{self.ID},{Associate.myAUTH}".encode("utf_8")
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(Associate.ADDR)
@@ -277,7 +345,6 @@ class Client:
     sendthread.start()
     recievethread.join()
     sendthread.join()
-
   def FINDID(self,TGTID,Associate,History = None):
     NEXTID = Associate.ID
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -286,12 +353,11 @@ class Client:
     client.send(packit)
     res = client.recv(2040).decode("utf-8")
     return res
-
-  def __init__(self,ID,ADDR,database = Database()):
+  def __init__(self,ID,ADDR,NAME = None, database = Database()):
     self.ID = ID
     self.ADDR = ADDR
+    self.NAME = NAME
     self.Database = database
-
   def REG(self,Associate,AUTH): 
     ip,port = self.ADDR
     myAUTH = AUTH
@@ -335,27 +401,23 @@ class Peer:
     self.ID = ID
     self.ADDR = ADDR
     self.Database = Database()
-    self.server = Server(self.ID,self.ADDR,self.Database)##copy of or is?
+    self.server = Server(self.ID,self.ADDR,self.Database)##IT IS SAME INSTANCE
     self.client = Client(self.ID,self.ADDR,self.Database)
     self.server.client = self.client
     self.client.server = self.server
     self.server.client.peer = self##allows peer methods and attributes to be acceced from server and client sub instances
     self.client.server.peer = self#strange recursion?
-
   def CHANGEID(self,ID):##needs more thought, much more thought
     if self.client.CHANGEID(ID):
       self.ID = ID
       self.client.ID = ID
       self.server.ID = ID
-
   def start(self):
     t1 = threading.Thread(target = self.server.handle_client)
     t1.start()
-
   @property
   def info(self):
     return Associate(ID = self.ID, ADDR = self.ADDR)
-
   def testAssociate(self, Associate):
     pass
   def testAllAssociates(self, lst = Database):
